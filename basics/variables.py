3ebe9ed30c4bd1bc50a4676344d1bc35dc5294cb
Tomas P <tomas.pultinevicius@visma.com>
2016-03-14 20:20:34 +0200

    added examples of string and list

--- basics/variables.py
+++ basics/variables.py
@@ -1,3 +1,4 @@
+print("-- integer -----------------------------")
 i = 5
 print("i = " + str(i))
 
@@ -5,3 +6,22 @@ i = i + 5
 print("i = i + 5");
 print("i = " + str(i));
 
+print("\n-- decimal -----------------------------")
+print("the // division is more than two times as fast! it truncates numbers")
+d = 10 / 3
+print(d)
+
+d = 10 // 3
+print(d)
+
+print("\n-- string ------------------------------")
+text = "Lists can be accessed via indexes"
+print("Text: " + text + ". Length: " + str(len(text)))
+print("text[0] + text[1] + text[22] + text[11] = " + text[0] + text[1] + text[22] + text[11])
+
+print("\n-- lists -------------------------------")
+capitals = ["Vienna", "London", "Paris", "Berlin", "Zurich", "Vilnius", "Hamburg"]
+print(capitals)
+print("capitals[5] = " + capitals[5])
+print("capitals[-2] = " + capitals[-2])
+print("Nummber of capitals: " + str(len(capitals)))
