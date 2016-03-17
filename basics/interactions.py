e974d09e6313a91ff11429d149e7c0c3d9c24fc6
pultitom <t.pultinevicius@gmail.com>
2016-03-17 20:56:44 +0200

    user input interactions

---
+++ basics/interactions.py
@@ -0,0 +1,7 @@
+name = input("What's your name? ")
+print("Nice to meet you " + name + "!\n")
+
+cities_lithuania = input("Largest cities in Lithuania: ")
+print(cities_lithuania, type(cities_lithuania))
+cities_lithuania = eval(input("Largest cities in Lithuania (example: [\"Vilnius\", \"Kaunas\"]): "))
+print(cities_lithuania, type(cities_lithuania), "\n")
